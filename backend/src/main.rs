4fe4d23a35f712206ed5349fc556e009d6d65cd2
TrailingWhitespace <flash1719@protonmail.com>
2026-08-16 19:12:57 +0530

    setup cors, allowed methods and headers for axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,15 @@
-use axum::{Router, response::Html, routing::get};
+use axum::{Router, response::Html, routing::get, http::HeaderValue};
+use tower_http::cors::{CorsLayer, Any};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/", get(handler)).layer(
+            CorsLayer::new()
+                .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+                .allow_methods(Any)
+                .allow_headers(Any)
+        );
 
-    // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
         .await
         .unwrap();
