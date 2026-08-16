d57ce1f83d2f79fed4fd31a470a3f45d314dd821
TrailingWhitespace <flash1719@protonmail.com>
2026-08-16 18:23:06 +0530

    Split into frontend and backend, update .gitignore

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new().route("/", get(health));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await;
+}
+
+async fn health() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
