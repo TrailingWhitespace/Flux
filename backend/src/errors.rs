e1563235a0bba4bd445de05b5d7b318aaa0c4872
TrailingWhitespace <flash1719@protonmail.com>
2026-08-17 22:32:19 +0530

    create a errors module
    create an enum for an error type FluxError with two variants for now
    a database error which is just a turso::Error
    implement IntoResponse for FluxError so they can be returned directly from axum routes
    implement From trait for turso::Error so it can be converted to FluxError by using `?` operator (currently used in todos route)

---
+++ backend/src/errors.rs
@@ -0,0 +1,26 @@
+use axum::{http::StatusCode, response::IntoResponse};
+
+#[allow(dead_code)]
+pub enum FluxError {
+    DatabaseError(turso::Error),
+    NotFound,
+}
+
+impl IntoResponse for FluxError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            FluxError::DatabaseError(e) => {
+                eprintln!("Database error: {:?}", e);
+                (StatusCode::INTERNAL_SERVER_ERROR, "Database error.").into_response()
+            }
+            FluxError::NotFound => (StatusCode::NOT_FOUND, "Not found.").into_response(),
+        }
+    }
+}
+
+// lets `?` auto-convert turso::Error into FluxError
+impl From<turso::Error> for FluxError {
+    fn from(e: turso::Error) -> Self {
+        FluxError::DatabaseError(e)
+    }
+}
