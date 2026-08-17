bb026f4b0a510d825d6e8b4ba90b11ad448d56d4
TrailingWhitespace <flash1719@protonmail.com>
2026-08-17 22:40:40 +0530

    initialize database and better router
    init_db creates an in memory turso database and returns the connection
    the connection is passed to axum routes to be used, through with_state(conn)

---
+++ backend/src/database.rs
@@ -0,0 +1,21 @@
+use turso::{Builder, Connection};
+
+pub async fn init_database() -> Result<Connection, Box<dyn std::error::Error>> {
+    let db = Builder::new_local(":memory:").build().await?;
+    let conn = db.connect()?;
+
+    
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo text, completed boolean, completedAt INTEGER);",
+        ()
+    ).await?;
+
+    // test insert
+    conn.execute(
+        "INSERT INTO todos (todo, completed) VALUES (?1, ?2);",
+        ("Do stuff", false),
+    )
+    .await?;
+
+    Ok(conn)
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,38 +1,45 @@
-use axum::{
-    Json, Router,
-    http::HeaderValue,
-    response::{Html, IntoResponse},
-    routing::get,
-};
+mod database;
+mod errors;
+mod models;
+mod routes;
+
+use axum::{Router, http::HeaderValue, response::Html, routing::get};
 use tower_http::cors::{Any, CorsLayer};
 
+use database::init_database;
+use routes::todos::todos_router;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // dyn std::error::Error is trait object (a way to represent any object that implements a specific trait),
+    // dyn is dynamic dispatch when at runtime converts any type of error
+    // for example turso::Error into a dyn Error which is then allocated on the heap using Box<> since we dont know the size of
+    // the error at compile time
+
+    let conn = init_database().await?;
     let app = Router::new()
         .route("/", get(check_health))
-        .route("/todos", get(fetch_todos))
+        .nest("/todos", todos_router())
         .layer(
             CorsLayer::new()
                 .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
                 .allow_methods(Any)
                 .allow_headers(Any), // Set specific methods and headers later
-        );
+        )
+        .with_state(conn);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
         .await
         .unwrap();
+
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app)
         .await
         .expect("Failed to start server");
+
+    Ok(())
 }
 
 async fn check_health() -> Html<&'static str> {
     Html("<h1>Works!</h1>")
-}
-
-async fn fetch_todos() -> impl IntoResponse {
-    //anything that implements IntoResponse can be returned
-    let dummy = Json([1, 2, 3, 4, 5]); // no need to call json! since Json seems to automatically serialize it
-    return dummy;
 }
