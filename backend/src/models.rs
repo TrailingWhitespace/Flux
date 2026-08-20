e63a67930e89ac96544e75e6c9c57247f34c01ab
TrailingWhitespace <flash1719@protonmail.com>
2026-08-21 02:33:41 +0530

    changes
    delete todo route
    impl tryfrom for Todo to make converting db rows into Todo structs

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,4 +1,6 @@
+use crate::errors;
 use serde::{Deserialize, Serialize};
+use turso::Row;
 
 #[allow(dead_code)]
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -7,4 +9,21 @@ pub struct Todo {
     pub todo: String,
     pub completed: bool,
     pub completed_at: i64,
+}
+
+impl TryFrom<&Row> for Todo {
+    type Error = errors::FluxError;
+
+    fn try_from(row: &Row) -> Result<Self, Self::Error> {
+        Ok(Todo {
+            id: row.get_value(0)?.as_integer().unwrap_or(&0).clone(),
+            todo: row
+                .get_value(1)?
+                .as_text()
+                .unwrap_or(&String::new())
+                .clone(),
+            completed: row.get_value(2)?.as_integer().unwrap_or(&0) != &0,
+            completed_at: row.get_value(3)?.as_integer().unwrap_or(&0).clone(),
+        })
+    }
 }
--- backend/src/routes/todos.rs
+++ backend/src/routes/todos.rs
@@ -12,6 +12,7 @@ pub fn todos_router() -> Router<Connection> {
     Router::new()
         .route("/", get(fetch_todos))
         .route("/insert_todo", post(insert_todo))
+        .route("/delete_todo", post(delete_todo))
 }
 
 pub async fn fetch_todos(State(conn): State<Connection>) -> Result<Json<Vec<Todo>>, FluxError> {
@@ -52,16 +53,7 @@ pub async fn insert_todo(
         .await?;
 
     if let Some(row) = rows.next().await? {
-        let todo: Todo = Todo {
-            id: row.get_value(0)?.as_integer().unwrap_or(&0).clone(),
-            todo: row
-                .get_value(1)?
-                .as_text()
-                .unwrap_or(&String::new())
-                .clone(),
-            completed: row.get_value(2)?.as_integer().unwrap_or(&0) != &0,
-            completed_at: row.get_value(3)?.as_integer().unwrap_or(&0).clone(),
-        };
+        let todo = Todo::try_from(&row)?;
         // println!("{:?}", todo);
         Ok(Json(todo)) // return only id or the entire Todo?
     } else {
@@ -71,7 +63,31 @@ pub async fn insert_todo(
     }
 }
 
+pub async fn delete_todo(
+    State(conn): State<Connection>,
+    Json(payload): Json<DeleteTodoRequest>,
+) -> Result<Json<Todo>, FluxError> {
+    let mut rows: Rows = conn
+        .query("DELETE FROM todos WHERE id = ? RETURNING *;", [payload.id])
+        .await?;
+
+    if let Some(row) = rows.next().await? {
+        let todo = Todo::try_from(&row)?;
+        println!("{:?}", todo);
+        Ok(Json(todo))
+    } else {
+        Err(FluxError::CustomError(String::from(
+            "Failed to delete todo.",
+        )))
+    }
+}
+
 #[derive(Debug, Deserialize)]
 pub struct CreateTodoRequest {
     pub todo: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DeleteTodoRequest {
+    pub id: i64,
 }
