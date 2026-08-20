9cfffa7f800c56f80f5dcdb872a78583e0e5dbc7
TrailingWhitespace <flash1719@protonmail.com>
2026-08-21 03:17:12 +0530

    toggle and update todo routes, toggle takes params and cleaner payload types using a generic IdRequest type alias, also add chrono for completedAt timestamp

--- backend/src/routes/todos.rs
+++ backend/src/routes/todos.rs
@@ -1,8 +1,9 @@
 use axum::{
     Json, Router,
-    extract::State,
+    extract::{Path, State},
     routing::{get, post},
 };
+use chrono::Utc;
 use serde::Deserialize;
 use turso::{Connection, Rows};
 
@@ -13,6 +14,8 @@ pub fn todos_router() -> Router<Connection> {
         .route("/", get(fetch_todos))
         .route("/insert_todo", post(insert_todo))
         .route("/delete_todo", post(delete_todo))
+        .route("/{id}/toggle", post(toggle_todo)) // params instead of body
+        .route("/update_todo", post(update_todo))
 }
 
 pub async fn fetch_todos(State(conn): State<Connection>) -> Result<Json<Vec<Todo>>, FluxError> {
@@ -21,16 +24,7 @@ pub async fn fetch_todos(State(conn): State<Connection>) -> Result<Json<Vec<Todo
     let mut todos: Vec<Todo> = vec![];
 
     while let Some(row) = rows.next().await? {
-        let todo = Todo {
-            id: row.get_value(0)?.as_integer().unwrap_or(&0).clone(),
-            todo: row
-                .get_value(1)?
-                .as_text()
-                .unwrap_or(&String::new())
-                .clone(),
-            completed: row.get_value(2)?.as_integer().unwrap_or(&0) != &0,
-            completed_at: row.get_value(3)?.as_integer().unwrap_or(&0).clone(),
-        };
+        let todo = Todo::try_from(&row)?;
         todos.push(todo);
     }
 
@@ -48,7 +42,7 @@ pub async fn insert_todo(
     let mut rows: Rows = conn
         .query(
             "INSERT INTO todos (todo, completed) VALUES (?, ?) RETURNING *;",
-            (payload.todo, 0),
+            (payload.todo, 0), // using a tuple because the values are of different data types (string, i64) so an array cant be used here
         )
         .await?;
 
@@ -73,7 +67,6 @@ pub async fn delete_todo(
 
     if let Some(row) = rows.next().await? {
         let todo = Todo::try_from(&row)?;
-        println!("{:?}", todo);
         Ok(Json(todo))
     } else {
         Err(FluxError::CustomError(String::from(
@@ -82,12 +75,70 @@ pub async fn delete_todo(
     }
 }
 
+pub async fn toggle_todo(
+    State(conn): State<Connection>,
+    Path(id): Path<i64>,
+) -> Result<Json<Todo>, FluxError> {
+    let mut rows: Rows = conn
+        .query(
+            "
+    UPDATE todos
+    SET
+      completed = 1 - completed,
+      completedAt = CASE
+        WHEN completed = 0 THEN ?
+        ELSE NULL
+      END
+    WHERE id = ?
+    RETURNING *;
+  ",
+            [Utc::now().timestamp_millis(), id],
+        )
+        .await?;
+
+    if let Some(row) = rows.next().await? {
+        let todo = Todo::try_from(&row)?;
+        Ok(Json(todo))
+    } else {
+        Err(FluxError::NotFound)
+    }
+}
+
+pub async fn update_todo(
+    State(conn): State<Connection>,
+    Json(payload): Json<UpdateTodoRequest>,
+) -> Result<Json<Todo>, FluxError> {
+    let mut rows: Rows = conn
+        .query(
+            "UPDATE todos SET todo = ? WHERE id = ? RETURNING *;",
+            (payload.todo, payload.id),
+        )
+        .await?;
+
+    if let Some(row) = rows.next().await? {
+        let todo = Todo::try_from(&row)?;
+        Ok(Json(todo))
+    } else {
+        Err(FluxError::NotFound)
+    }
+}
+
+// Validator to check lengths and such?
 #[derive(Debug, Deserialize)]
 pub struct CreateTodoRequest {
     pub todo: String,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct DeleteTodoRequest {
+pub struct UpdateTodoRequest {
+    pub id: i64,
+    pub todo: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct IdRequest {
     pub id: i64,
 }
+
+pub type DeleteTodoRequest = IdRequest;
+// pub type ToggleTodoRequest = IdRequest;
