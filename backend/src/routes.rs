9603205b202a792930902e914e03053d30777570
TrailingWhitespace <flash1719@protonmail.com>
2026-09-15 02:01:15 +0530

    basic globals.css, yet to add a default theme in :root a home page which is page.tsx at the root with its Home.css file a todos route

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,1 +1,2 @@
 pub mod todos;
+// maybe do this the routes/mod.rs way so src/ is less confusing?
---
+++ frontend/src/app/todos/page.tsx
@@ -0,0 +1,7 @@
+export default function TodosPage() {
+  return (
+    <main>
+      <h1>this is todos yes</h1>
+    </main>
+  );
+}
