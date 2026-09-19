eea9b139f4c7bd123bb7dc047650c20ee4efb46c
TrailingWhitespace <flash1719@protonmail.com>
2026-09-20 02:26:22 +0530

    use an env variable for mobile ip for testing and add framer motion

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
+  allowedDevOrigins: process.env.LOCAL_DEV_IP ? [process.env.LOCAL_DEV_IP] : [],
 };
 
 export default nextConfig;
